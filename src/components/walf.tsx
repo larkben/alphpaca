c49e7abac2846e865fccb3c024d69868cfa23ef9
Benjamin Larkey <60051088+larkben@users.noreply.github.com>
2024-05-21 10:14:29 -0500

    readability update

--- src/components/walf.tsx
+++ src/components/walf.tsx
@@ -71,10 +71,10 @@ export const WrappedAlfDapp: FC<{
     <main className="flex flex-col items-center justify-center min-h-screen py-20 bg-gray-900 sm:px-6 lg:px-8">
       <div className="min-w-30 space-y-8 bg-gray-800 p-6 rounded-lg shadow-md z-50">
         <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-orange-500">Wrapped OG ALF Protocol *NOT LIVE* </h2>
-          <p className="mt-2 text-center text-sm text-gray-400"> This is a wrapper for your OG Alf. This contract protects your OG Alf and allows for redemptions. <br/> The ratio is: 10000:1 <br/> Therefore the total maximum supply of wrapped OG Alf is 1,000,000.1000 </p>
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-orange-500">Wrapped OG ALF Protocol NOW LIVE! </h2>
+          <p className="mt-2 text-center text-sm text-gray-400"> This is a wrapper for your OG Alf. This contract protects your OG Alf and allows for redemptions. <br/> The ratio is: 1:1 <br/> Trading is accomplished with more decimal precision. </p>
         </div>
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-orange-500"> Mint Wrapped Alf </h2>
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-orange-500"> Mint OG Wrapped Alf </h2>
         <form  onSubmit={handleMintWrappedOgAlf} className="mt-8 space-y-6">
           <div className="rounded-md shadow-sm -space-y-px">
             <div>
