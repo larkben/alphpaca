c01a05ef798be4558e48e45de55b36798124b552
Benjamin Larkey <60051088+larkben@users.noreply.github.com>
2024-09-11 20:02:46 -0500

    ui update

--- src/app/battle/page.tsx
+++ src/app/battle/page.tsx
@@ -2,16 +2,14 @@ import Image from "next/image";
 import { Navbar } from "../../components/navbar";
 import { TokenCreate } from "../../services/utils";
 import { AlephiumConnectButton, AlephiumWalletProvider } from '@alephium/web3-react'
-import { SimplifiedNftProfileViewer } from "../../components/simplified-nft-profile-viewer"
-import React from "react";
-import Dashboard from "../../components/profile";
+import { EnhancedNftBattleWindow } from "../../components/enhanced-nft-battle-window";
 
 export default function TokenCPage() {
   return (
     <main>
       <AlephiumWalletProvider network={"mainnet"}>
         <Navbar/>
-        <Dashboard/>
+        <EnhancedNftBattleWindow/>
       </AlephiumWalletProvider>
     </main>
   );
