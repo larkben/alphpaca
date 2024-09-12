5067a8c671c21e8f6d6e837be43a8bc4cef9a343
larkben <spartinofarrows@gmail.com>
2024-09-11 19:23:57 -0500

    mui ui

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,14 +2,14 @@ import Image from "next/image";
 import { Navbar } from "../../components/navbar";
 import { TokenCreate } from "../../services/utils";
 import { AlephiumConnectButton, AlephiumWalletProvider } from '@alephium/web3-react'
-import SimplifiedNftProfileViewer from "../../components/simplified-nft-profile-viewer"
+import Dashboard from "../../components/profile";
 
 export default function TokenCPage() {
   return (
     <main>
       <AlephiumWalletProvider network={"mainnet"}>
         <Navbar/>
-        <SimplifiedNftProfileViewer/>
+        <Dashboard/>
       </AlephiumWalletProvider>
     </main>
   );
--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -1,55 +1,34 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as TabsPrimitive from "@radix-ui/react-tabs"
+import * as React from "react";
+import * as Tabs from "@radix-ui/react-tabs";
 
-import { cn } from "../../../lib/utils"
+const TabExample = () => (
+  <Tabs.Root className="w-full" defaultValue="tab1">
+    <Tabs.List className="flex space-x-1 bg-gray-100 p-1 rounded-xl">
+      <Tabs.Trigger value="tab1" className="px-4 py-2 text-sm font-medium text-center text-gray-600 bg-white shadow rounded-lg focus:outline-none">
+        Tab 1
+      </Tabs.Trigger>
+      <Tabs.Trigger value="tab2" className="px-4 py-2 text-sm font-medium text-center text-gray-600 hover:bg-white hover:shadow rounded-lg focus:outline-none">
+        Tab 2
+      </Tabs.Trigger>
+      <Tabs.Trigger value="tab3" className="px-4 py-2 text-sm font-medium text-center text-gray-600 hover:bg-white hover:shadow rounded-lg focus:outline-none">
+        Tab 3
+      </Tabs.Trigger>
+    </Tabs.List>
 
-const Tabs = TabsPrimitive.Root
+    <Tabs.Content value="tab1" className="p-4">
+      <p>Content for Tab 1</p>
+    </Tabs.Content>
 
-const TabsList = React.forwardRef<
-  React.ElementRef<typeof TabsPrimitive.List>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.List
-    ref={ref}
-    className={cn(
-      "inline-flex h-10 items-center justify-center rounded-md bg-gray-100 p-1 text-gray-500 dark:bg-gray-800 dark:text-gray-400",
-      className
-    )}
-    {...props}
-  />
-))
-TabsList.displayName = TabsPrimitive.List.displayName
+    <Tabs.Content value="tab2" className="p-4">
+      <p>Content for Tab 2</p>
+    </Tabs.Content>
 
-const TabsTrigger = React.forwardRef<
-  React.ElementRef<typeof TabsPrimitive.Trigger>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.Trigger
-    ref={ref}
-    className={cn(
-      "inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-white transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-white data-[state=active]:text-gray-950 data-[state=active]:shadow-sm dark:ring-offset-gray-950 dark:focus-visible:ring-gray-300 dark:data-[state=active]:bg-gray-950 dark:data-[state=active]:text-gray-50",
-      className
-    )}
-    {...props}
-  />
-))
-TabsTrigger.displayName = TabsPrimitive.Trigger.displayName
+    <Tabs.Content value="tab3" className="p-4">
+      <p>Content for Tab 3</p>
+    </Tabs.Content>
+  </Tabs.Root>
+);
 
-const TabsContent = React.forwardRef<
-  React.ElementRef<typeof TabsPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
->(({ className, ...props }, ref) => (
-  <TabsPrimitive.Content
-    ref={ref}
-    className={cn(
-      "mt-2 ring-offset-white focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-gray-950 focus-visible:ring-offset-2 dark:ring-offset-gray-950 dark:focus-visible:ring-gray-300",
-      className
-    )}
-    {...props}
-  />
-))
-TabsContent.displayName = TabsPrimitive.Content.displayName
-
-export { Tabs, TabsList, TabsTrigger, TabsContent }
+export default TabExample;
