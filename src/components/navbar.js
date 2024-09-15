c04ac19967d471856755030f84218533449c21b0
OverGames <30346630+OverGamesDev@users.noreply.github.com>
2024-09-15 04:50:02 +0200

    ANS integration + paca balance

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,19 +5,40 @@ import { AlephiumConnectButton, useBalance, useWallet } from '@alephium/web3-rea
 import { AiOutlineCopy, AiOutlineUser } from "react-icons/ai";
 import { PiWalletBold } from "react-icons/pi";
 import { LiaSignOutAltSolid } from 'react-icons/lia';
+import { ANS } from '@alph-name-service/ans-sdk';
+import { balanceOf } from "../lib/utils";
 
 export function Navbar() {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isWalletDropdownOpen, setIsWalletDropdownOpen] = useState(false);
   const wallet = useWallet();
   const { balance } = useBalance();
-  const [walletAddress, setWalletAddress] = useState('');
+  const [ansName, setAnsName] = useState('');
+  const [ansUri, setAnsUri] = useState('');
+  const [pacaBalance, setPacaBalance] = useState(0);
 
   useEffect(() => {
     if (wallet?.account?.address) {
-      setWalletAddress(wallet.account.address);
-    }
-  }, [wallet?.account?.address]);
+        const getProfile = async () => {
+          const ans = new ANS('mainnet');
+          const testProfile = await ans.getProfile(wallet?.account?.address);
+          if (testProfile?.name) {
+              setAnsName(testProfile.name);
+          }
+          if (testProfile?.imgUri) {
+              setAnsUri(testProfile.imgUri);
+          }
+        };
+      getProfile();
+      
+      const fetchPacaBalance = async () => {
+        const pacaTokenId = "b2d71c116408ae47b931482a440f675dc9ea64453db24ee931dacd578cae9002";
+        const balance = await balanceOf(pacaTokenId, wallet.account?.address);
+        setPacaBalance(balance);
+      };
+      fetchPacaBalance();
+      }
+}, [wallet?.account?.address]);
 
   const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
   const toggleWalletDropdown = () => setIsWalletDropdownOpen(!isWalletDropdownOpen);
@@ -69,7 +90,11 @@ export function Navbar() {
               return isConnected ? (
                 <div className="relative">
                   <button onClick={toggleWalletDropdown} className="text-white hover:text-cyan-300 focus:outline-none p-2">
+                  {ansUri ? (
+                    <img src={ansUri}  width={38} height={38} className="rounded-full w-10 h-w-10 shadow dark:shadow-gray-700" alt=""  />
+                  ) : (
                     <AiOutlineUser className="w-8 h-8" />
+                  )}
                   </button>
                   {isWalletDropdownOpen && (
                     <div className="absolute right-0 mt-2 w-48 rounded overflow-hidden bg-slate-800/80 shadow shadow-gray-600">
@@ -77,24 +102,26 @@ export function Navbar() {
                       <div className="py-8 bg-gradient-to-tr from-cyan-300/80 to-purple-400/80"></div>
                         <div className="absolute px-4 -bottom-3 start-0">
                           <div className="flex items-end">
-                            <AiOutlineUser className="w-8 h-8" />
+                            {ansUri ? (
+                              <img src={ansUri}  width={38} height={38} className="rounded-full w-10 h-w-10 shadow dark:shadow-gray-700" alt=""  />
+                            ) : (
+                              <AiOutlineUser className="w-8 h-8" />
+                            )}
 
-                            {/*<img src={ansUri || "/images/client/01.jpg"}  width={38} height={38} className="rounded-full w-10 h-w-10 shadow dark:shadow-gray-700" alt=""  />*/}
-
-                            <span className="font-semibold text-[15px] ms-1">No Name</span>
+                            <span className="font-semibold text-[15px] ms-1">{ansName || "No Name"}</span>
                           </div>
                         </div>
                       </div>
                       <div className="p-4">
                         <h5 className="font-semibold text-[15px]">Wallet:</h5>
                         <div className="flex items-center justify-between">
-                          <span className="text-[13px] text-slate-400">{shortenHash(walletAddress)}</span>
+                          <span className="text-[13px] text-slate-400">{shortenHash(wallet?.account?.address)}</span>
                           <button onClick={() => copyToClipboard(walletAddress)} className="text-cyan-300"><AiOutlineCopy/></button>
                         </div>
                       </div>
                       <div className="p-4">
                         <h5 className="text-[15px]">Balance: <span className="text-cyan-300 font-semibold">{(parseFloat(bal) / 1e18).toFixed(2)} $ALPH</span></h5>
-                        <h5 className="text-[15px]">PACA: <span className="text-cyan-300 font-semibold">{(parseFloat(bal) / 1e18).toFixed(2)} $PACA</span></h5>
+                        <h5 className="text-[15px]">PACA: <span className="text-cyan-300 font-semibold">{(parseFloat(pacaBalance) / 1e18).toFixed(2)} $PACA</span></h5>
                       </div>
                       <div className="border-t border-gray-100 dark:border-gray-800"></div>
                       <button onClick={disconnect} className="w-full text-left px-4 py-2 text-[14px] font-semibold hover:text-cyan-300">
