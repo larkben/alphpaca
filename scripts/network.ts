9a452b7ff20bdc6a8d85b91cec187a75ee298fa8
larkben <spartinofarrows@gmail.com>
2024-09-17 19:02:37 -0500

    cleaned for repo re-init

--- scripts/network.ts
+++
@@ -1,6 +0,0 @@
-import { NetworkId, ONE_ALPH } from "@alephium/web3"
-
-export function getNetwork(): NetworkId {
-    const network = (process.env.NEXT_PUBLIC_NETWORK ?? 'testnet') as NetworkId
-    return network
-}
