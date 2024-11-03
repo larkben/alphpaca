a70c79a05bff8b974d41bbee8b3c8258ffdb9c18
Benjamin Larkey <spartinofarrows@gmail.com>
2024-11-03 10:28:27 -0600

    bug: deployment scripts not working

--- alephium.config.ts
+++ alephium.config.ts
@@ -1,35 +1,51 @@
-import { Configuration } from '@alephium/cli'
-import { Number256 } from '@alephium/web3'
+const Configuration = require('@alephium/cli')
+const Number256 = require('@alephium/web3')
 
 const dotenv = require('dotenv');
 dotenv.config()
 
-// Settings are usually for configuring
-export type Settings = {
-  issueTokenAmount: Number256
-}
-const defaultSettings: Settings = { issueTokenAmount: BigInt(100) }
+let config: typeof Configuration;
 
-const configuration: Configuration<Settings> = {
+export type Settings = {
+  commissionRate: number // basis point. e.g. 200: 2%
+}
+
+export function loadSettings(network: 'devnet' | 'testnet' | 'mainnet'): { commissionRate: number } {
+  return {
+    commissionRate: 100, // 1%
+  }
+}
+
+const configuration: typeof config = {
+  deploymentScriptDir: 'scripts',
+  compilerOptions: {
+    errorOnWarnings: false,
+    ignoreUnusedConstantsWarnings: true,
+  },
+
   networks: {
     devnet: {
+      networkId: 4,
       nodeUrl: 'http://localhost:22973',
       privateKeys: [
-        'a642942e67258589cd2b1822c631506632db5a12aabcf413604e785300d762a5' // group 0
+        'a642942e67258589cd2b1822c631506632db5a12aabcf413604e785300d762a5'
       ],
-      settings: defaultSettings
+      confirmations: 1,
+      settings: loadSettings('devnet')
     },
 
     testnet: {
-      nodeUrl: (process.env.NODE_URL as string) ?? 'https://wallet-v20.testnet.alephium.org',
+      nodeUrl: process.env.NODE_URL as string ?? 'https://wallet-v20.testnet.alephium.org',
       privateKeys: process.env.key === undefined ? [] : process.env.key.split(','),
-      settings: defaultSettings
+      confirmations: 2,
+      settings: loadSettings('testnet')
     },
 
     mainnet: {
-      nodeUrl: (process.env.NODE_URL as string) ?? 'https://wallet-v20.mainnet.alephium.org',
+      nodeUrl: process.env.NODE_URL as string ?? 'https://wallet-v20.mainnet.alephium.org',
       privateKeys: process.env.prodkey === undefined ? [] : process.env.prodkey.split(','),
-      settings: defaultSettings
+      confirmations: 2,
+      settings: loadSettings('mainnet')
     }
   }
 }
